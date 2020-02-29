04352510ff124bff436f9869cd815c9816624e84
caveofjulian <julian.feenstra0@gmail.com>
2020-02-29 14:18:58 +0100

    changed descriptive analysis especially to be more decoupled

--- descriptive-analysis/descriptive_analysis.R
+++ descriptive-analysis/descriptive_analysis.R
@@ -1,31 +1,57 @@
+library(psych)
+
 # setup -------------------------------------------------------------------
-
 df <- getDataFrame()
-# Likert scale variables are not included because they consist of multiple variables
-quantitive_variables <- c("HrPWork", "HrVWork", "HrClass", "HrStud")
-
-
-# Quantitive generic analysis ---------------------------------------------
-
-# In this analysis there are no likert scale variables because they are measured with multiple variables.
-# Please visit cronbach_analysis to gain its analysis
-export_quantitive_analysis <- function(data_frame) {
-  analysis <- describe(data_frame[quantitive_variables], skew = F)
-  export_all_data(analysis, file_name = "quantitive_analysis")
-}
-
-export_quantitive_analysis(df) 
-
-# Outliers analysis --------------------------------------------------------
 # Assigning all of them to a variable so we don't have to access it every time, also these variable names are 
-# much more descriptive so we can understand the code.
+# much more descriptive and is a way to decouple the code from the dataframe.
 paid_hours <- df$HrPWork
 volunteer_hours <- df$HrVWork
 class_hours <- df$HrClass
 study_hours <- df$HrStud
 
-# All variables share the same range. Although paid_hours ends at 59 we want steps of 10, so this is acceptable.
-range <- c(0, 60)
+# Likert scale variables are not included because they consist of multiple variables
+quantitive_variables <- c("HrPWork", "HrVWork", "HrClass", "HrStud")
+range <- c(0, 60) # All variables share the same range. 
+
+# Only 0.03% of the data should have a z-score of 3 or higher. We use this z-score to look for the outliers, 
+# however, the results will be analyzed in the report to determine whether or not these are legit outliers and should
+# be removed.
+outliers_zscore <- 3
+
+
+# Export summaries of descriptive analysis --------------------------------
+# In this analysis there are no likert scale variables because they are measured with multiple variables.
+# Please visit cronbach_analysis to gain its analysis
+export_quantitive_analysis <- function(data_frame, file_name="quantitive_analysis") {
+  analysis <- describe(data_frame[quantitive_variables], skew = F)
+  export_all_data(analysis, file_name = file_name)
+}
+
+# Note: in the report we compare the means from the analysis above (WITH outliers) to this analyis (WITHOUT outliers)
+export_quantitive_analysis_without_outliers <- function(data, file_name="quantative_analysis_without_outliers") {
+  # We must ensure that data is of type list because we're unlisting its indexes. This is important because we don't 
+  # want de tightly couple the this method to the variable naming and having to access it by that (having as a result
+  # that we wouldn't be able to reuse it when we change the Z scores!). C will automatically bundle all vectors provided
+  # into one vector, meaning that accessing index 1 won't return the first VECTOR, but the first actual number.
+  if(!is.list(data)) {
+    stop("data must be of type list!")
+  }
+  
+  # Because we are not dealing with a dataframe (dataframes MUST have the same amount of rows), we have to make an
+  # analysis for each separately, at least as far I know at this point. Maybe look back at this when we've got time?
+  paid_hours_analysis <- describe(unlist(data[0]), skew = F)
+  volunteer_hours_analyis <- describe(unlist(data[1]), skew = F)
+  class_hours_analyis <- describe(unlist(data[2]), skew = F)
+  study_hours_analyis <- describe(unlist(data[3]), skew = F)
+  
+  # Making this a list so it's being accepted as a sequence, basically a necessity due to the comment above
+  export_all_data(list(paid_hours_analysis, volunteer_hours_analyis, class_hours_analyis, study_hours_analyis), 
+                  file_name)
+}
+
+# Quantitive analysis (including outliers analysis) -----------------------
+# Exports an analysis of the quantitive variables in the dataframe.
+export_quantitive_analysis(df) 
 
 hist(paid_hours, col = "Blue", ylim = range, xlab="Paid time (in hours/week)", las=1)
 hist(volunteer_hours, col = "Blue", ylim = range, xlab="Volunteer time (in hours/week)", las=1)
@@ -42,10 +68,6 @@ volunteer_hours_zscores <- scale(volunteer_hours)
 class_hours_zscores <- scale(class_hours)
 study_hours_zscores <- scale(study_hours)
 
-# Only 0.03% of the data should have a z-score of 3 or higher. We use this z-score to look for the outliers, 
-# however, the results will be analyzed in the report to determine whether or not these are legit outliers and should
-# be removed.
-outliers_zscore <- 3
 
 paid_hours_outliers <- paid_hours[which(abs(paid_hours_zscores) > outliers_zscore)]
 volunteer_hours_outliers <- volunteer_hours[which(abs(volunteer_hours_zscores) > outliers_zscore)]
@@ -57,31 +79,26 @@ volunteer_hours_without_outliers <- volunteer_hours[-which(abs(volunteer_hours_z
 class_hours_without_outliers <- class_hours[-which(abs(class_hours_zscores) > outliers_zscore)]
 study_hours_without_outliers <- study_hours[-which(abs(study_hours_zscores) > outliers_zscore)]
 
-
-# Note: the old means are part of the quantitive analysis that's been exported before.  
-export_quantitive_analysis_different_row_count <- function(data_frame, data) {
-  # Because we are not dealing with a dataframe (dataframes MUST have the same amount of rows), we have to make an
-  # analysis for each separately, at least as far I know at this point. Maybe look back at this when we've got time?
-  paid_hours_analysis <- describe(data[1], skew = F)
-  volunteer_hours_analyis <- describe(data[2], skew = F)
-  class_hours_analyis <- describe(data[3], skew = F)
-  study_hours_analyis <- describe(data[4], skew = F)
-  
-  # Making this a list so it's being accepted as a sequence, basically a necessity due to the comment above
-  export_all_data(list(paid_hours_analysis, volunteer_hours_analyis, class_hours_analyis, study_hours_analyis), 
-                  file_name = "quantitive_analysis_without_outliers")
-}
- mean(study_hours[-study_hours_extremes], na.rm = TRUE)
+mean(study_hours[-study_hours_extremes], na.rm = TRUE)
 
 data_without_outliers <- list(paid_hours_without_outliers, volunteer_hours_without_outliers, class_hours_without_outliers,
                            study_hours_without_outliers)
-export_quantitive_analysis_without_outliers(df[quantitive_variables], data_without_outliers)
 
+# Exports all the relevant information to a file called quantitive analysis without outliers
+export_quantitive_analysis_without_outliers(data_without_outliers)
 
-paid_hours_mean_without_outliers <- mean(paid_hours[-paid_hours_extremes], na.rm = TRUE)
-volunteer_hours_mean_without_outliers <- mean(volunteer_hours[-volunteer_hours_extremes], na.rm = TRUE)
-class_hours_mean_without_outliers <- mean(class_hours[-class_hours_extremes], na.rm = TRUE)
-study_hours_mean_without_outliers <-
+# A Z-Score of 3 turned out to be very low for paid hours and volunteer hours, we want to look for > 45 for paid
+# hours and for volunteer hours to > 20.
+paid_hours_without_outliers_revisited <- paid_hours[-which(abs(paid_hours) > 40)]
+volunteer_hours_without_outliers_revisited <- volunteer_hours[-which(abs(volunteer_hours) > 20)]
+
+data_without_outliers_revisited <- 
+  list(paid_hours_without_outliers_revisited, volunteer_hours_without_outliers_revisited,
+       class_hours_without_outliers, study_hours_without_outliers) 
+
+export_quantitive_analysis_without_outliers(data_without_outliers_revisited, 
+                                            file_name = "quantative_analysis_without_outliers_revisited")
+
 
 
 # Should become a package -------------------------------------------------
